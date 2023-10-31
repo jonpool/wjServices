2447cb19f0047ad05e4ca019d9dd2b30aeb19446
jonpool <joaopl_4@hotmail.com>
2023-10-30 20:10:53 -0400

    mainpageFixed

--- src/components/Pages/Home3.jsx
+++ src/components/Pages/Home3.jsx
@@ -93,42 +93,6 @@ class Home3 extends React.Component {
         );
     };
 };
-var emailjs="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js"
 
-
-(function email () {
-        emailjs.init("ErtuG5RWbvTQ40Que");
-      })();
-        // listen to the form submission
-        document
-          .getElementById("contact-form")
-          .addEventListener("submit", function (event) {
-            event.preventDefault();
-  
-            const serviceID = "service_6koh0hb";
-            const templateID = "template_e6w0wlw";
-            let nome = document.getElementById("name").value;
-            let email = document.getElementById("email").value;
-            let phoneNumber = document.getElementById("phoneNumber").value;
-						let message = document.getElementById("message").value;
-						console.log(message);
-            var contactParam ={
-                from_nome:nome,
-                from_email:email,
-                from_telefone:phoneNumber,
-								from_message:message
-            }
-            // send the email here
-            emailjs.send(serviceID, templateID, contactParam).then(
-              (response) => {
-                console.log("SUCCESS!", response.status, response.text);
-                alert("SUCCESSO - Seu email foi registrado!");
-              },
-              (error) => {
-                console.log("FAILED...", error);
-                alert("FAILED...", error);
-              }
-            );
-          });
 
 export default Home3;
