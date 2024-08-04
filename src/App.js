1dd702a634af30ccd5fc452445ed39947d328538
jonpool <joaopl_4@hotmail.com>
2024-08-03 21:01:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Components from './components/Components';
+import Facebook from "./tracking/Facebook";
 import ReactGA from 'react-ga';
 const TRACKING_ID = "G-4M18S05XNT"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
@@ -9,6 +10,7 @@ const App = () => {
   
   return (
     <div className="App">
+      <Facebook />
       <Components />  
    </div>
   );
